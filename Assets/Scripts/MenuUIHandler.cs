b23a45bed98fa41970672f6078e05a431c4fd6a3
brunomena <brunogrejanin@hotmail.com>
2021-10-07 13:14:01 -0300

    Persistence between scenes OK

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuUIHandler : MonoBehaviour
 {
+    public GameObject fooText;
+    Text footext;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        footext = fooText.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NewText()
+    {
+        // add code here to handle when a color is selected
+        MenuManager.Instance.playerName = footext.text;
+        Debug.Log(footext.text);
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
---
+++ Assets/Scripts/UIMainScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMainScene : MonoBehaviour
+{
+    public GameObject fooText;
+    Text footext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        footext = fooText.GetComponent<Text>();
+        footext.text = MenuManager.Instance.playerName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
