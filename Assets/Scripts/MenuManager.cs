8182a84bee8f57561ea7287d20c036927c20db06
brunomena <brunogrejanin@hotmail.com>
2021-10-09 18:22:45 -0300

    Final
    Everything working as it should

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.IO;
 
 
 public class MenuManager : MonoBehaviour
@@ -25,12 +25,15 @@ public class MenuManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        if (BestScore == "")
+        LoadScore();
+
+        if (BestScore == null)
         {
             BestScore = "0";
         }
 
         BestScoreText = "Best Score: ";
+
     }
     void Start()
     {
@@ -43,4 +46,32 @@ public class MenuManager : MonoBehaviour
 
     }
 
+    [System.Serializable]
+    class SaveData
+    {
+        public string BestScore;
+        public string playerNameBestScore;
+    }
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.BestScore = BestScore;
+            data.playerNameBestScore = playerNameBestScore;
+
+        string json = JsonUtility.ToJson(data);
+  
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerNameBestScore = data.playerNameBestScore;
+            BestScore = data.BestScore;
+        }
+    }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -38,6 +38,8 @@ public class MenuUIHandler : MonoBehaviour
     }
     public void Exit()
     {
+        MenuManager.Instance.SaveScore();
+
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
